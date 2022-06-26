0f7fdeca29fce0c2c437f688ce0eca9bf8e37d36
ilya-skoropad <ilya.skoropad0@gmail.com>
2022-06-26 20:18:02 +0300

    added some work

--- web/index.js
+++ web/index.js
@@ -1,14 +1,26 @@
 const http = require('http');
-
-const hostname = '127.0.0.1';
+const { Client } = require('pg')
 const port = 3000;
+
+const client = new Client({
+  user: 'postgres',
+  host: '127.0.0.1:5432',
+  database: 'app',
+  port: 5432,
+});
+
+let msg = '';
+client.connect(function(err) {
+  if (err) msg = 'error';
+  msg = 'connected';
+});
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  res.end(msg);
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(port, () => {
+  console.log(`Server running`);
 });
